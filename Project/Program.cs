7a6233706d1de2f67ac5ab1aef3203ff1b9c0c95
Artem Topchii <artem.t@vec-ua.com>
2022-06-25 15:39:17 +0300

    added task 3

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,6 +7,8 @@ namespace Project
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
